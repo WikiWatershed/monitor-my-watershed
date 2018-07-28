f6cdbfb5dfbfe1a028b43fecb4760b3ac52795c3
jcaraballo17 <juan.caraballo17@gmail.com>
2018-07-28 08:28:37 -0400

    Updated tsa catalog rebuild script

--- src/dataloaderinterface/management/commands/rebuild_tsa_catalog.py
+++ src/dataloaderinterface/management/commands/rebuild_tsa_catalog.py
@@ -1,11 +1,21 @@
+import json
+import os
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import connection, connections, transaction
 
+from dataloader.models import Organization, Variable, Unit, Result
 from dataloaderinterface.models import SiteSensor
 
 
 class Command(BaseCommand):
     help = ''
+    server = ''
+    database_server = ''
+    organization_descriptions = {}
+    variable_types = {}
+    unit_types = {}
+    values_count = {}
 
     insert_query = 'INSERT INTO public."DataSeries"({}) VALUES ({});'
     data_series_columns = (
@@ -18,11 +28,22 @@ class Command(BaseCommand):
         "NoDataValue"
     )
 
-    wofpy_get_url = 'http://envirodiysandbox.usu.edu/wofpy/rest/1_1/GetValues' \
+    wofpy_get_url = '{server}/wofpy/rest/1_1/GetValues' \
                     '?location={sampling_feature_code}&variable={variable_code}&methodCode=2' \
                     '&sourceCode={organization_id}&qualityControlLevelCode=Raw&startDate=&endDate='
-    influx_get_url = 'http://wpfdbssandbox.uwrl.usu.edu:8086/query?u=web_client&p=password&db=envirodiy' \
+    influx_get_url = '{database_server}:8086/query?u=web_client&p=password&db=envirodiy' \
                      '&q=SELECT%20time,%20DataValue::field,%20UTCOffset::field%20FROM%20{influx_identifier}'
+
+    def retrieve_server_data(self):
+        try:
+            with open(os.path.join(settings.BASE_DIR, 'settings', 'settings.json')) as data_file:
+                data = json.load(data_file)
+
+            self.server = data['host']
+            self.database_server = next(db_connection for db_connection in data['databases'] if db_connection.name == 'tsa_catalog')
+        except IOError:
+            print("Error reading server configuration data (settings.json)")
+
 
     def build_query_string(self):
         columns_string = (','.join(['"%s"'] * len(self.data_series_columns))) % self.data_series_columns
@@ -31,8 +52,8 @@ class Command(BaseCommand):
 
     def get_data_series_data(self, sensor):
         print('- getting data series values for sensor {}'.format(sensor.sensor_identity))
+
         registration = sensor.registration
-        result = sensor.result
         return (
             self.get_source_id(sensor),  # -> SourceDataServiceID
             self.get_network(sensor),  # -> Network
@@ -45,66 +66,122 @@ class Command(BaseCommand):
             sensor.sensor_output.variable_name,  # -> VariableName
             self.get_variable_level(sensor),  # -> VariableLevel
             sensor.sensor_output.unit_name,  # -> VariableUnitsName
-            sensor.sensor_output.unit.unit_type_id,  # -> VariableUnitsType
+            self.unit_types[sensor.sensor_output.unit_id],  # -> VariableUnitsType
             sensor.sensor_output.unit_abbreviation,  # -> VariableUnitsAbbreviation
             sensor.sensor_output.sampled_medium,  # -> SampleMedium
             self.get_value_type(sensor),  # -> ValueType
-            result.timeseriesresult.aggregation_statistic_id,  # -> DataType
-            sensor.sensor_output.variable.variable_type_id,  # -> GeneralCategory
-            result.processing_level.processing_level_code,  # -> QualityControlLevelCode
-            result.processing_level.definition,  # -> QualityControlLevelDefinition
-            result.processing_level.explanation or '',  # -> QualityControlLevelExplanation
-            registration.organization_name,  # -> SourceOrganization
-            registration.organization.organization_description,  # -> SourceDescription
+            self.get_data_type(sensor),  # -> DataType
+            self.variable_types[sensor.sensor_output.variable_id],  # -> GeneralCategory
+            self.get_processing_level_code(sensor),  # -> QualityControlLevelCode
+            self.get_processing_level_definition(sensor),  # -> QualityControlLevelDefinition
+            self.get_processing_level_explanation(sensor),  # -> QualityControlLevelExplanation
+            registration.organization_name or '',  # -> SourceOrganization
+            self.organization_descriptions[registration.organization_id] if registration.organization_id else '',  # -> SourceDescription
             registration.registration_date,  # -> BeginDateTime
             self.get_utc_offset(sensor),  # -> UTCOffset
-            result.value_count,  # -> NumberObservations
+            self.values_count[sensor.result_id],  # -> NumberObservations
             sensor.last_measurement.value_datetime,  # -> DateLastUpdated
             self.get_is_active(sensor),  # -> IsActive
             self.wofpy_get_url.format(  # -> GetDataURL
+                server=self.server,
                 sampling_feature_code=registration.sampling_feature_code,
                 variable_code=sensor.sensor_output.variable_code,
                 organization_id=registration.organization_id
             ),
-            self.influx_get_url.format(influx_identifier=self.get_influx_identifier(sensor)),  # -> GetDataInflux
+            self.influx_get_url.format(
+                database_server=self.database_server,
+                influx_identifier=self.get_influx_identifier(sensor)
+            ),  # -> GetDataInflux
             str(sensor.result_uuid),  # -> ResultUUID
             self.get_influx_identifier(sensor),  # -> InfluxIdentifier
             self.get_no_data_value(sensor)  # -> NoDataValue
         )
 
+
+
     def get_influx_identifier(self, sensor):
         return 'uuid_{}'.format(str(sensor.result_uuid).replace('-', '_'))
 
+    def get_processing_level_explanation(self, sensor):
+        """ Hook for someone to somehow figure out the right QualityControlLevelExplanation"""
+        return ''
+
+    def get_processing_level_definition(self, sensor):
+        """ Hook for someone to somehow figure out the right QualityControlLevelDefinition"""
+        return 'Raw Data'
+
+    def get_processing_level_code(self, sensor):
+        """ Hook for someone to somehow figure out the right QualityControlLevelCode"""
+        return 'Raw'
+
+    def get_data_type(self, sensor):
+        """ Hook for someone to somehow figure out the right DataType"""
+        return 'Average'
+
     def get_no_data_value(self, sensor):
-        """I have to finish this as fast as i can, so here's a hook to somehow figure out the right NoDataValue"""
+        """ Hook for someone to somehow figure out the right NoDataValue"""
         return -9999
 
     def get_is_active(self, sensor):
-        """I have to finish this as fast as i can, so here's a hook to somehow figure out the right IsActive"""
+        """ Hook for someone to somehow figure out the right IsActive"""
         return 1
 
     def get_utc_offset(self, sensor):
-        """I have to finish this as fast as i can, so here's a hook to somehow figure out the right UTCOffset"""
+        """ Hook for someone to somehow figure out the right UTCOffset"""
         return 0
 
     def get_source_id(self, sensor):
-        """I have to finish this as fast as i can, so here's a hook to somehow figure out the right SourceDataServiceID"""
+        """ Hook for someone to somehow figure out the right SourceDataServiceID"""
         return 1
 
     def get_network(self, sensor):
-        """I have to finish this as fast as i can, so here's a hook to somehow figure out the right Network"""
+        """ Hook for someone to somehow figure out the right Network"""
         return 'EnviroDIY'
 
     def get_variable_level(self, sensor):
-        """I have to finish this as fast as i can, so here's a hook to somehow figure out the right VariableLevel"""
+        """ Hook for someone to somehow figure out the right VariableLevel"""
         return 'Common'
 
     def get_value_type(self, sensor):
-        """I have to finish this as fast as i can, so here's a hook to somehow figure out the right ValueType"""
+        """ Hook for someone to somehow figure out the right ValueType"""
         return 'Field Observation'
 
+    def load_odm2_related_data(self, sensors):
+        organization_ids = [id[0] for id in sensors.values_list('registration__organization_id')]
+        self.organization_descriptions = {
+            id: definition
+            for (id, definition)
+            in Organization.objects.filter(organization_id__in=organization_ids).values_list('organization_id', 'organization_description')
+        }
+
+        variable_ids = [id[0] for id in sensors.values_list('sensor_output__variable_id')]
+        self.variable_types = {
+            id: variable_type
+            for (id, variable_type)
+            in Variable.objects.filter(variable_id__in=variable_ids).values_list('variable_id', 'variable_type_id')
+        }
+
+        unit_ids = [id[0] for id in sensors.values_list('sensor_output__unit_id')]
+        self.unit_types = {
+            id: unit_type
+            for (id, unit_type)
+            in Unit.objects.filter(unit_id__in=unit_ids).values_list('unit_id', 'unit_type_id')
+        }
+
+        result_ids = [id[0] for id in sensors.values_list('result_id')]
+        self.values_count = {
+            id: value_count
+            for (id, value_count)
+            in Result.objects.filter(result_id__in=result_ids).values_list('result_id', 'value_count')
+        }
+
     def handle(self, *args, **options):
-        sensors = SiteSensor.objects.filter(last_measurement__isnull=False, sensor_output__isnull=False)[:2]
+        sensors = SiteSensor.objects.select_related('registration').prefetch_related('last_measurement', 'sensor_output')\
+            .filter(last_measurement__isnull=False, sensor_output__isnull=False)\
+            .defer('height', 'sensor_notes', 'registration__site_notes')
+
+        self.retrieve_server_data()
+        self.load_odm2_related_data(sensors)
         data_series = tuple(self.get_data_series_data(sensor) for sensor in sensors)
         print('* {} data series collected'.format(len(data_series)))
 
