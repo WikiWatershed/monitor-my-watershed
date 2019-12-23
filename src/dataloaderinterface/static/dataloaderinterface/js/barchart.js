830f8d1bac5e1d059cdc382e8778cf926c5f526e
htaolimno <htaolimno@users.noreply.github.com>
2019-12-23 18:14:47 -0500

    taxonomic group legend new style
    #408 : the legend for the taxonomic groups chart waslengthy. Heather sugguested moving it underneath the chart and split into columns. Three columns are used to shorten the legend.

--- src/dataloaderinterface/static/dataloaderinterface/js/barchart.js
+++ src/dataloaderinterface/static/dataloaderinterface/js/barchart.js
@@ -51,15 +51,26 @@ $(".taxon").each(function () {
     total += count;
 });
 
-var legendContainer = $("#legend-container table");
+var legendContainert0 = $("#legend-container-t0 table");
+var legendContainert1 = $("#legend-container-t1 table");
+var legendContainert2 = $("#legend-container-t2 table");
 
 // Compute percentages
 for (var i = 0; i < data.length; i++) {
     data[i].frequency = data[i].frequency / total;
     var f = isNaN(data[i].frequency) ? "0" : (data[i].frequency * 100).toFixed(2);
+    var id = i % 3;
+    var $container;
 
+    if (id===0) {
+        $container = legendContainert0;
+    } else if (id===1) {
+        $container = legendContainert1;
+    } else {
+        $container = legendContainert2;
+    }
     // Populate legend container
-    legendContainer.append(
+    $container.append(
         '<tr>' +
         '<td><i style="color: ' + c20(i) + '" class="fa fa-square mdl-list__item-icon" aria-hidden="true"></i></td>' +
         '<td class="mdl-data-table__cell--non-numeric">' +
