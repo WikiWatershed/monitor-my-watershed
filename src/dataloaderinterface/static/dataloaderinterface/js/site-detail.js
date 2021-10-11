be3b817761cc09fdb688c79e3b15c6e03f63e520
Paul Tomasula <31142705+ptomasula@users.noreply.github.com>
2021-10-11 18:06:17 -0400

    Fix day and month formatting bug

--- src/dataloaderinterface/static/dataloaderinterface/js/site-detail.js
+++ src/dataloaderinterface/static/dataloaderinterface/js/site-detail.js
@@ -25,8 +25,8 @@ function initMap() {
 
 function format_date(date) {
     year = String(date.getFullYear()).padStart(4, '0');
-    month = String(date.getMonth()).padStart(2, '0');
-    day = String(date.getDay()).padStart(2, '0');
+    month = String(date.getMonth()+1).padStart(2, '0');
+    day = String(date.getDate()).padStart(2, '0');
     hour = String(date.getHours()).padStart(2, '0');
     minute = String(date.getMinutes()).padStart(2, '0');
     second = String(date.getSeconds()).padStart(2, '0');
