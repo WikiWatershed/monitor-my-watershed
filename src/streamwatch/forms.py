2d94ebca79589cf5a394a5fdba7ff9d65788ad01
Hua Tao <htao@limno.com>
2022-06-07 12:34:42 -0400

    added javascript file to streamwatch app

--- src/streamwatch/forms.py
+++ src/streamwatch/forms.py
@@ -17,14 +17,40 @@ class StreamWatchForm(forms.Form):
     
     ACTIVITY_TYPE_CHOICES = (('chemical', 'Chemical Action Team'), ('biological', 'Biological Action Team'), ('baterial', 'Baterial Action Team'))
 
+    weather_condition_choices = (
+        (1, 'Clear'),
+        (2, 'Partly cloudy'),
+        (3, 'Overcast')
+    )
+    water_color_choices = ((1,'Clear'),
+        (2,'Green'),
+        (3,'Blue-green'),
+        (4,'Brown'),
+        (5,'Yellow'),
+        (6,'Gray'),
+        (7,'Other'))
+
+    water_odor_choices = (
+        (1,'Normal'),
+        (2,'Anaerobic'),
+        (3,'Sulfuric'),
+        (4,'Sewage'),
+        (5,'Petroleum'),
+        (6,'Other'))
+
+    place_holder_choices = (
+        (1, 'Choice #1'), 
+        (2, 'Choice #2'),
+        (3, 'Choice #3')
+    )
     def __init__(self, *args, **kwargs):
         super(StreamWatchForm, self).__init__(*args, **kwargs)
         #self.fields['types'].initial = self.ACTIVITY_TYPE_CHOICES_LIST
     
-    invesgator1 = forms.DateField(
+    invesgator1 = forms.CharField(
         label='Invesgator #1'
     )       
-    invesgator2 = forms.DateField(
+    invesgator2 = forms.CharField(
         label='Invesgator #2'
     )   
     collect_date = forms.DateField(
@@ -41,13 +67,74 @@ class StreamWatchForm(forms.Form):
     #     queryset=LeafPackType.objects.filter(created_by=None),
     # )
     
+    # mutiple choices
     activity_types = forms.MultipleChoiceField(
         widget=MDLCheckboxSelectMultiple,
         label='Activity type(s):',
         required=True,
         choices = ACTIVITY_TYPE_CHOICES)
     
+    # floating number input
     air_temp = forms.FloatField(
         label='Air Temperature',
         required=False,
+    )
+    
+    # single choice
+    weather_cond = forms.ChoiceField(
+        required=False,
+        widget=forms.Select,
+        label='Current Weather Conditions:',
+        choices= weather_condition_choices,
+        initial='1'
+    )
+    time_since_last_precip = forms.ChoiceField(
+        required=True,
+        widget=forms.Select,
+        label='Time Since Last Rain or Snowmelt:',
+        choices= place_holder_choices,
+        initial='1'
+    )
+    
+    water_color = forms.ChoiceField(
+        required=True,
+        widget=forms.Select,
+        label='Water Color:',
+        choices= water_color_choices,
+        initial='1'
+    )
+    water_odor = forms.MultipleChoiceField(
+        widget=MDLCheckboxSelectMultiple,
+        required=True,
+        label='Water Odor:',
+        choices= water_odor_choices,
+    )
+    
+    turbidity = forms.ChoiceField(
+        required=True,
+        widget=forms.Select,
+        label='Turbidity:',
+        choices= place_holder_choices,
+        initial='1'
+    )
+    
+    water_movement = forms.ChoiceField(
+        required=True,
+        widget=forms.Select,
+        label='Turbidity:',
+        choices= place_holder_choices,
+        initial='1'
+    )
+    aquatic_vegetation = forms.ChoiceField(
+        required=True,
+        widget=forms.Select,
+        label='Aquatic Vegetation:',
+        choices= place_holder_choices,
+        initial='1'
+    )
+    surface_coating = forms.MultipleChoiceField(
+        widget=MDLCheckboxSelectMultiple,
+        required=True,
+        label='Surface Coating:',
+        choices= place_holder_choices,
     )
