1c061b2f202aec5c07c91facef998337c3d520f1
Paul Tomasula <31142705+ptomasula@users.noreply.github.com>
2022-06-08 12:15:40 -0400

    Change varaible_choice_options fields

--- src/streamwatch/models.py
+++ src/streamwatch/models.py
@@ -3,12 +3,11 @@ odm2_engine = odm2datamodels.odm2_engine
 odm2_models = odm2datamodels.models
 
 import sqlalchemy
-import pandas as pd
 
 from typing import Dict, Any, Iterable, Tuple
 
 def variable_choice_options(variable_domain_cv:str) -> Iterable[Tuple]:
-    query = (sqlalchemy.select(odm2_models.Variables.variableid, odm2_models.Variables.variablenamecv)
+    query = (sqlalchemy.select(odm2_models.Variables.variablecode, odm2_models.Variables.variabledefinition)
             .where(odm2_models.Variables.variabletypecv == variable_domain_cv)
         )
     records = odm2_engine.read_query(query, output_format='records')
