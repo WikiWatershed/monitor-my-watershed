68e0bb2dd1e902cca8ab3a3f2936c421c4bc804a
Anthony Aufdenkampe <aaufdenkampe@limno.com>
2022-08-08 15:21:49 -0500

    Improve `timeutils`; shorten tz list
    @ptomasula, this update is primarily to shorten the timezone list from 596 to 46. It also generalizes the two functions so that we can eventually feed them local time and timezone info from the user's web browser.

--- src/streamwatch/timeutils.py
+++ src/streamwatch/timeutils.py
@@ -3,50 +3,129 @@ from math import trunc
 from typing import Tuple, List
 import zoneinfo
 
-# Calculate local current UTC Offset
-def get_utcoffset(tz_key: str = None) -> Tuple[int, str, str]:
+# Calculate UTC Offset for a tz & dt
+def get_utcoffset(tz_key: str = None, 
+                  dt: datetime = None) -> Tuple[int, str, str]:
     '''
-    Calculate standard time UTC offset integer and short name 
-    for a given IANA timezone key.
+    Calculate standard time UTC offset integer, offset timedelta string, 
+    and short name for Standard Time for a given IANA timezone key and 
+    a given datetime.
     
-    tz_key: if None it is based on timezone information
-    from the operating system.
+    tz_key: a valid `zoneinfo` timezone key. 
+    If `None` it is based on timezone information from the operating system.
+
+    dt: the datetime used to determine the UTC offset for a given timezone, 
+    considering daylight savings vs. standard time. 
+    If `None` it is assumed to be January 1st 2022, to return Standard Time in the northern hemisphere
     
-    WARNING: Conversion from daylight savings to standard time
+    WARNING: This providesConversion from daylight savings to standard time
     will not likely work in the southern hemisphere. 
     '''
+    if not dt:
+        dt = datetime.now()
 
-    dt_winter = datetime(2022, 1, 1)
     if not tz_key:
-        # Get local timzone
-        dt_now = dt_winter.astimezone() 
+        # Get local timezone
+        dt_tz = dt.astimezone() 
     else:
-        # Get local timzone
-        dt_now = dt_winter.astimezone(zoneinfo.ZoneInfo(tz_key)) 
+        # Get local timezone
+        dt_tz = dt.astimezone(zoneinfo.ZoneInfo(tz_key)) 
+
     
-    utc_offset = dt_now.utcoffset().total_seconds() / (60*60)
+    utc_offset = dt_tz.utcoffset().total_seconds() / (60*60)
     utc_offset_int = trunc(utc_offset)
     
-    return utc_offset_int, dt_now.isoformat()[-6:], dt_now.tzname()
+    return utc_offset_int, dt_tz.isoformat()[-6:], dt_tz.tzname()
 
 
-def make_tz_tuple_list() -> List:
+def make_tz_tuple_list(tz_key_list: list = None,
+                       dt: datetime = None) -> list:
     '''
     Creates a list of tuples with:
     - `zoneinfo` timezone keys
-    - display field for timezone info
+    - display field for timezone info, which is a concatenation of:
+      - UTC offset in hours
+      - `zoneinfo` timezone key
+      - `zoneinfo` timezone name/code, which often distinguishes
+      daylight savings (D) vs. standard time (S).
+
+    tz_key_list: a supplied list of `zoneinfo` timezone keys.
+        If None, then uses all >500 `zoneinfo` timezone keys 
     '''
     tz_tuple_list = []
-    for tz_key in zoneinfo.available_timezones():
-        tz_info = get_utcoffset(tz_key)
-        tz_display = f"UTC{tz_info[1]} {tz_key} ({tz_info[2]})"
+
+    if not tz_key_list:
+        tz_key_list = zoneinfo.available_timezones()
+    else:
+        tz_key_list
+    
+    if not dt:
+        dt = datetime.utcnow().astimezone(zoneinfo.ZoneInfo('UTC'))
+
+    for tz_key in tz_key_list:
+        tz_info = get_utcoffset(tz_key, dt)
+        tz_display = f"(UTC{tz_info[1]}) {tz_key} ({tz_info[2]})"
         tz_tuple = (tz_key, tz_display)
         tz_tuple_list.append(tz_tuple)
     
     def item2(tz_tuple):
         return get_utcoffset(tz_tuple[0])[0]
+
+    tz_tuple_list_sorted = tz_tuple_list.sort(reverse=True, key=item2)
     
-    tz_tuple_list.sort(reverse=True, key=item2)
     return tz_tuple_list
-    
-    
+
+
+   # A very short list of all whole-hour UTC offsets, 
+# using biggest cities, with and without daylight savings
+# NOTE: many timezones have half-hour UTC offsets (and also 15 min);
+# these fractional-hour offsets will not work with ODM2.0
+tz_key_shortlist = [
+    'Pacific/Kiritimati',
+    'Pacific/Auckland',
+    'Pacific/Tongatapu',
+    'Pacific/Fiji',
+    'Australia/Sydney',
+    'Pacific/Noumea',
+    'Asia/Vladivostok',
+    'Pacific/Guam',
+    'Asia/Tokyo',
+    'Pacific/Palau',
+    'Asia/Shanghai',
+    'Asia/Singapore',
+    'Australia/Perth',
+    'Asia/Bangkok',
+    'Asia/Dhaka',
+    'Asia/Karachi',
+    'Asia/Dubai',
+    'Asia/Istanbul',
+    'Europe/Moscow',
+    'Africa/Nairobi',
+    'Europe/Athens',
+    'Asia/Jerusalem',
+    'Africa/Douala',
+    'Europe/Paris',
+    'Africa/Brazzaville',
+    'Europe/London',
+    'UTC',
+    'Atlantic/Cape_Verde',
+    'Atlantic/South_Georgia',
+    'America/Sao_Paulo',
+    'America/Halifax',
+    'America/Manaus',
+    'US/Eastern',
+    'America/Bogota',
+    'US/Central',
+    'Pacific/Galapagos',
+    'US/Mountain',
+    'Etc/GMT+7',
+    'US/Pacific',
+    'Pacific/Pitcairn',
+    'US/Alaska',
+    'Pacific/Gambier',
+    'US/Hawaii',
+    'Pacific/Rarotonga',
+    'Pacific/Samoa',
+    'Pacific/Niue',
+    'Etc/GMT+12',
+]    
