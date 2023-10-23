a60f7fdc4c88be91e648ab5398dcec1bc3128282
Paul Tomasula <31142705+ptomasula@users.noreply.github.com>
2023-10-23 10:22:30 -0400

    Address scrolling past popup

--- src/dataloaderinterface/static/dataloaderinterface/js/index.js
+++ src/dataloaderinterface/static/dataloaderinterface/js/index.js
@@ -10,6 +10,26 @@ function initSubscriptionDialog() {
         height: 650,
         modal: true,
         resizable: true,
-        title:'Upcoming Changes'
+        title:'Upcoming Changes',
+        open: function() {
+            //hide the x on the dialog title
+            $(this).parent().find(".ui-dialog-titlebar-close").hide();
+
+            //disable scrolling (hide overflow) 
+            $('body').css({overflow: 'hidden'});
+        },
+        close: function() {
+            //disable scrolling (hide overflow) 
+            $('body').css({overflow: 'auto'});
+
+        },
+        buttons: [
+            {
+                text: 'Close',
+                click: function() {
+                    $(this).dialog('close');
+                }
+            }
+        ]
     })
 } 
